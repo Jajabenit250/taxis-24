e45a388ebd3bc597e1c64b48ef788e56f896decf
Jajabenit250 <jajabenit@gmail.com>
2020-10-05 18:34:50 +0200

    seed added

--- src/database/seeders/20201005150343-demo-location.js
+++ src/database/seeders/20201005150343-demo-location.js
@@ -1,24 +1,73 @@
-'use strict';
-
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  up: (queryInterface, Sequelize) =>
+    queryInterface.bulkInsert("Locations", [
+      {
+        latitude: -1.9389523,
+        longitude: 30.0869735,
+        country: "Rwanda",
+        city: "Gisozi",
+        state: "Umujyi wa Kigali",
+        streetName: "KG 569 Street",
+        streetNumber: "KG 569 Street",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        latitude: -1.9351704,
+        longitude: 30.0884943,
+        country: "Rwanda",
+        city: "Gisozi",
+        state: "Umujyi wa Kigali",
+        streetName: "KG 587 Street",
+        streetNumber: "KG 587 Street",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        latitude: -1.9353499,
+        longitude: 30.0870413,
+        country: "Rwanda",
+        city: "Gisozi",
+        state: "Umujyi wa Kigali",
+        streetName: "KG 585 Street",
+        streetNumber: "KG 585 Street",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        latitude: -1.9345445,
+        longitude: 30.0874302,
+        country: "Rwanda",
+        city: "Gisozi",
+        state: "Umujyi wa Kigali",
+        streetName: "KG 585 Street",
+        streetNumber: "KG 585 Street",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        latitude: -1.9313093,
+        longitude: 30.1374922,
+        country: "Rwanda",
+        city: "Kinyinya",
+        state: "Umujyi wa Kigali",
+        streetName: "KG 20 Street",
+        streetNumber: "KG 20 Street",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        latitude: -1.9365969,
+        longitude: 30.1320304,
+        country: "Rwanda",
+        city: "Kinyinya",
+        state: "Umujyi wa Kigali",
+        streetName: "KG 21 Street",
+        streetNumber: "KG 21 Street",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]),
+  down: (queryInterface, Sequelize) =>
+    queryInterface.bulkDelete("Locations", null, {}),
 };
